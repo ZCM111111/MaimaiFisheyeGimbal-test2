020f4ba2d2d24e8093bab11ac8a4744bbf83aa9d
93543 <93543@localhost>
2026-07-21 21:54:05 +0800

    fix: remove extra k3/k4 from LensProfile and SettingsView

--- MaimaiFisheyeStabilizer/Models/LensProfile.swift
+++ MaimaiFisheyeStabilizer/Models/LensProfile.swift
@@ -7,8 +7,6 @@ struct LensProfile: Codable, Equatable {
     var principalPointY: Float  // Lens center Y (pixels)
     var k1: Float               // Fisheye distortion coefficient k1
     var k2: Float               // Fisheye distortion coefficient k2
-    var k3: Float               // Fisheye distortion coefficient k3
-    var k4: Float               // Fisheye distortion coefficient k4
 
     static let `default` = LensProfile(
         name: "238° Fisheye",
@@ -16,9 +14,7 @@ struct LensProfile: Codable, Equatable {
         principalPointX: 960.0, // Half of 1920
         principalPointY: 540.0, // Half of 1080
         k1: 0.0,                // Will need calibration
-        k2: 0.0,
-        k3: 0.0,
-        k4: 0.0
+        k2: 0.0
     )
 
     // MARK: - Persistence
